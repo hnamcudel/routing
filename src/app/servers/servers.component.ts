d95427067f2d39a381eb2fccec407bd95f92ab8e
manhld1002 <ldmgithub2001@gmail.com>
2024-02-27 08:10:17 +0700

    guard

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { ServersService } from './servers.service';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+  public servers: {id: number, name: string, status: string}[] = [];
+
+  constructor(private serversService: ServersService, private router: Router, private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.servers = this.serversService.getServers();
+  }
+
+  onReload() {
+    // Inject the currently active route with {relativeTo: this.route}
+    // this.router.navigate(['servers'], {relativeTo: this.route});
+  }
+
+}
